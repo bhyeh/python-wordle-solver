2b69151698af6e123a6647a73f994e5bd456fce5
Benjamin <by253@cornell.edu>
2022-04-29 15:17:31 -0400

    rhodes push

--- entropybot.py
+++ entropybot.py
@@ -23,44 +23,53 @@ class EntropyBot(Bot):
 
     Parameters
     ----------
+    current_state : ndarray
+      remaining valid guesses
+
     idx : int
       integer indicating attempt number
-
-    word: str
-      guess at current attempt number
 
     Returns
     -------
     new_state : ndarray
+
+    Meeting Notes: 4/29/22
+    ----------------------
+    correct word: boats
+    Guess   Result/Information gained
+    meeep   we can exclude with those letters
+    barts   has mix of all cases; new_state all words w/ letters of b at position 1; 
+    ----------------------
     
     """
-
-    # Initialize new word state
-    new_state = []
 
     # Interpret gameboard
     game_app = self.driver.find_element(By.TAG_NAME , 'game-app')
     game_rows = self.driver.execute_script("return arguments[0].shadowRoot.getElementById('board')", game_app).find_elements(By.TAG_NAME, 'game-row')
     game_tiles = self.driver.execute_script('return arguments[0].shadowRoot', game_rows[idx]).find_elements(By.CSS_SELECTOR , 'game-tile')
-
+    # Initialize; correct, present, absent
+    correct = []
+    present = []
+    absent = []
     # Parse pattern
     for i, tile in enumerate(game_tiles):
       letter = tile.get_attribute('letter')
       eval = tile.get_attribute('evaluation')
       # Letter is present and at exact position in answer
       if eval == 'correct':
-        # Add words to new state with letter at position `i` in word
-        new_state += [word for word in current_state if word[i] == letter]
+        # Add words to new state with letter at POSITION `i` in word
+        correct += [word for word in current_state if word[i] == letter]
       # Letter is present in answer
       elif eval == 'present':
-        # Add words to new state with letter in word
-        new_state += [word for word in current_state if letter in word]
+        # Add words to new state WITH LETTER in word
+        present += [word for word in current_state if letter in word]
       # Letter is not present in answer
       else:
-        # Add words to new state without letter in word
-        new_state += [word for word in current_state if letter not in word ]
-    
-    return np.unique(new_state)
+        # Add words to new state WITHOUT LETTER in word
+        absent += [word for word in current_state if letter not in word ]
+    # New worde state is INTERSECTION of three states; (correct x present x absent)
+    new_state = list(set.intersection(*map(set, [correct, present, absent])))
+    return new_state
 
   def calculate_information_gain_aux(self, word):
     """"

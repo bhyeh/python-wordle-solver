42b46d83669db37dd1484811f78697dde904568c
Benjamin <by253@cornell.edu>
2022-05-01 22:32:08 -0400

    renamed PlayWordle

--- playwordle.py
+++ playwordle.py
@@ -1,7 +1,7 @@
 from random import Random
-from randombot import RandomBot
-from brutebot import BruteBot
-from entropybot import EntropyBot
+from RandomBot import RandomBot
+from BruteBot import BruteBot
+from EntropyBot import EntropyBot
 
 if __name__ == '__main__':
     # replace bot class with desired bot

59afdbeb41a523e767d44bd288d054737f6abd46
Benjamin <by253@cornell.edu>
2022-05-02 23:24:15 -0400

    grocery items TODO

--- Eval.py
+++ Eval.py
@@ -1,6 +1,18 @@
-"""
+# Import bots
+from RandomBot import RandomBot
+from BruteBot import BruteBot
 
+"""
+Goal:
+    -> Specify bot type; play game several times (100) and
+       evaluate performance
+    -> No. wins / no. attempts (accuracy)
+    -> No. guesses / no. attempts (average number attempts)
+
+Grocery items:
+    (1) Method to evaluate game whether WON or LOST
 
 """
 
 if __name__ == '__main__':
+    pass

3ad23004397c95c6d00fa18f4d7b2d045576b7f9
Benjamin <by253@cornell.edu>
2022-05-12 19:18:58 -0400

    skeleton for word-frequency bot

---
+++ FreqBot.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import numpy as np
+from numpy import random
+from time import sleep
+# Parent class
+from Bot import Bot
+
+class FreqBot(Bot):
+
+    """
+    \\TODO: Write class docstring
+    
+    """
+
+    def __init__(self):
+        """\\TODO: Write constructor docstring
+        
+        """
+        pass

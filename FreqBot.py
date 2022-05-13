0bf4dc6a197ca7effbf31b99e3488287b7900149
Benjamin <by253@cornell.edu>
2022-05-13 14:49:49 -0400

    skeleton; possibly TODO

---
+++ FreqBot.py
@@ -0,0 +1,5 @@
+    # Naïve approach — simply count in how many words does each letter appear, 
+    # and choose the word with the aggregate 
+    # highest number on its five letters. 
+    #   -> For each correct/present letter guessed; choose the next word 
+    #      with the highest number of each correct/present letter in it

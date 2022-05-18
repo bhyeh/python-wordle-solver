7ade58b8c80ace6b090b5cca9771a69b6cc1448d
Benjamin <by253@cornell.edu>
2022-05-17 20:47:13 -0400

    deleted freqbot

--- FreqBot.py
+++
@@ -1,5 +0,0 @@
-    # Naïve approach — simply count in how many words does each letter appear, 
-    # and choose the word with the aggregate 
-    # highest number on its five letters. 
-    #   -> For each correct/present letter guessed; choose the next word 
-    #      with the highest number of each correct/present letter in it

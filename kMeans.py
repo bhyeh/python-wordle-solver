11ba224fe36f6eaefe805fc2758f7c547679abde
Benjamin <by253@cornell.edu>
2022-05-17 20:47:42 -0400

    kMeans; TODO possibily

---
+++ kMeans.py
@@ -0,0 +1,17 @@
+# Import packages performing actions on Website
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+# Import aux functions
+import numpy as np
+from numpy import random
+from time import sleep
+# Parent class
+from Bot import Bot  
+
+class KMeansBot(Bot):
+
+  """A bot that makes attempts based on current word state.
+  
+  """
+
+  pass

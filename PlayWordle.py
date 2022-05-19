55c7bc30f78204da5f8db909ae8b57927a72e0df
Benjamin <by253@cornell.edu>
2022-05-19 02:17:07 -0400

    formated under new dir structure

--- PlayWordle.py
+++ PlayWordle.py
@@ -1,11 +1,11 @@
 # Import playable bots
-from RandomBot import RandomBot
-from ReduceBot import ReduceBot
-from ZipfBot import ZipfBot
-from EntropyBot import EntropyBot
+from Bots.RandomBot import RandomBot
+from Bots.ReduceBot import ReduceBot
+from Bots.ZipfBot import ZipfBot
+from Bots.EntropyBot import EntropyBot
 
 if __name__ == '__main__':
-    # Replace bot class with desired bot
+    # Replace bot class with desired bot (default Zipf)
     #   -> Check source code for constructor arguments (ZipfBot, EntropyBot)
     #      where computations are required for first runs
     bot = ZipfBot()
